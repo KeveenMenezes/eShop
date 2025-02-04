448a594b8c81e4afabb518042f72a76dd2eee299
Keveen Menezes <endykeven@gmail.com>
2025-02-03 22:08:30 -0300

    feat: implement handler and command for deleting a product

---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -0,0 +1,22 @@
+
+namespace Catalog.API.Products.DeleteProduct;
+
+public record DeleteProductCommand(Guid Id) 
+    : ICommand<DeleteProductResult>;
+
+public record DeleteProductResult(bool isSuccess);
+
+internal class DeleteProductHandler
+    (IDocumentSession session, ILogger<DeleteProductHandler> logger)
+    : ICommandHandler<DeleteProductCommand, DeleteProductResult>
+{
+    public async Task<DeleteProductResult> Handle(DeleteProductCommand request, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("Deleting product with id {Id}", request.Id);
+
+        session.Delete<Product>(request.Id);
+        await session.SaveChangesAsync(cancellationToken);
+
+        return new DeleteProductResult(true);
+    }
+}
