a12b54d9d20ac26d12de0cf58007ebd7133066bd
Keveen Menezes <endykeven@gmail.com>
2025-02-11 17:17:02 -0300

    refactor: simplify validation behavior by improving async task handling

--- src/BuildingBlocks/Behaviors/ValidationBehavior.cs
+++ src/BuildingBlocks/Behaviors/ValidationBehavior.cs
@@ -14,12 +14,12 @@ public class ValidationBehavior<TRequest, TResponse>
         var context = new ValidationContext<TRequest>(request);
 
         var validationResults = await Task.WhenAll(
-                validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+            validators.Select(v => v.ValidateAsync(context, cancellationToken)));
 
         var failures = validationResults
-                    .SelectMany(result => result.Errors)
-                    .Where(f => f != null)
-                    .ToList();
+            .SelectMany(result => result.Errors)
+            .Where(f => f != null)
+            .ToList();
 
         if (failures.Count != 0)
         {
